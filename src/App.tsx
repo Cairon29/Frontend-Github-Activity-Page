3c4fd71c82afa698c666c88a7489f922f36beefb
Cairon29 <josewjjunco@gmail.com>
2025-12-28 22:30:46 -0500

    feat: github OAuth test with appwrite library aded

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
+import { useEffect } from 'react'
 import { handleLogin } from './login'
+import { Client, Account } from 'appwrite'
 
-function App() {
-  return (
-    <>
-      <h1>Here the Github activity page Ur commit </h1>
-      <button id="login" onClick={handleLogin}>Login with Github</button>
-    </>
-  )
+  function App() {
+
+    useEffect(() => {
+        const client = new Client()
+          .setEndpoint('https://sfo.cloud.appwrite.io/v1')
+          .setProject('694c53ba002808c0b726');
+
+        const account = new Account(client);
+        account.get()
+          .then(user => {
+              console.log('Logged user:', user);
+          })
+          .catch(() => {
+              console.log('not logged yet');
+          });
+    }, [])
+
+    return (
+      <>
+        <h1>Here the Github activity page Ur commit </h1>
+        <button id="login" onClick={handleLogin}>Login with Github</button>
+      </>
+    )
 }
 
 export default App
--- src/login.tsx
+++ src/login.tsx
@@ -1,21 +1,27 @@
+import { Client, Account, OAuthProvider } from 'appwrite';
+
 export const handleLogin = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault()
 
     const loginButton = e.target as HTMLButtonElement
     loginButton.textContent = 'Logging in...'
+    try {
+        const client = new Client()
+            .setEndpoint('https://sfo.cloud.appwrite.io/v1')
+            .setProject('694c53ba002808c0b726')
+            .setLocale('en-US');
 
-    const response = await fetch('http://localhost:5555/api/auth/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            password: '123456',
-            user: 'test user name'
-        }),
-    })
-    if (response.ok) {
-        const data = await response.json()
-        loginButton.textContent = data
+        client.setCookieFallback(true);
+        const account = new Account(client);
+    
+        account.createOAuth2Session({
+            provider: OAuthProvider.Github,
+            success: 'http://localhost:1111/',
+            failure: 'http://localhost:1111/fail_login'
+        })
+
+    } catch (error) {
+        console.log(error)
+        loginButton.textContent = 'Login with Github'
     }
 }
